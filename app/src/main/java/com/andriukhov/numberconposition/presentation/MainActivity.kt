3129a3902e4a50f69c2e5fe21676e4781d9fdb99
Andriukhov Denys <andryhov.denis@gmail.com>
2022-03-10 00:25:29 +0300

    create WelcomeFragment, refactor dependencies

--- app/src/main/java/com/andriukhov/numberconposition/presentation/MainActivity.kt
+++ app/src/main/java/com/andriukhov/numberconposition/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.andriukhov.numberconposition.presentation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.andriukhov.numberconposition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, WelcomeFragment())
+            .commit()
     }
 }
