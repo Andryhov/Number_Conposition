12b26f8c8db0534261668c3b6fba43ea5625b463
Andriukhov Denys <andryhov.denis@gmail.com>
2022-03-14 00:25:10 +0300

    refactor

--- app/src/main/java/com/andriukhov/numberconposition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/andriukhov/numberconposition/presentation/WelcomeFragment.kt
@@ -6,14 +6,39 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.andriukhov.numberconposition.R
+import com.andriukhov.numberconposition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment: Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+    get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickButtonSubmit()
+    }
+
+    private fun clickButtonSubmit() {
+        binding.submitButton.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.container, LevelSelectionFragment.newInstance())
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding == null
     }
 }
